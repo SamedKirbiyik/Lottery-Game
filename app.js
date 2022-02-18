a57649676bc52cdcae5a43e802a4d680c4c66bb2
YilmazEsat <esaattyilmazzz>
2022-02-18 15:53:50 +0300

    add

---
+++ app.js
@@ -0,0 +1,5 @@
+let container = document.getElementById("container");
+let contentText = document.getElementById("contentText");
+let chanceNumber = document.getElementById("chanceNumber");
+let addButton = document.getElementById("addButton");
+let deleteButton = document.getElementById("deleteButton");
