572fe0a9b01c0ddc94f7db06f59d65f0e435a444
Samed <esaattyilmazzz>
2022-02-21 11:48:53 +0300

    added

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ let contentText = document.getElementById("contentText");
 let chanceNumber = document.getElementById("chanceNumber");
 let addButton = document.getElementById("addButton");
 let deleteButton = document.getElementById("deleteButton");
+let output = document.getElementById("output");
+
+addButton.addEventListener('click', function(){
+    
+})
+
+
+
+
+
+
+
+
+// değişkenler sonrasında sayaç sıfırlanır
+// user girişi maks 8 ile sınırlanır.
+// if bloğuyla toplam şans numarası 6 ile sınırlanır. Ve küçükten büyüğe sıralanır.
+// Sayaç ++ her girişte
+// bu numaraların 1 ile 90 arasında olması math metoduyla sağlanır.
+// 7. numaranın joker olması nedeniyle ilk 6 numarayla !=
+// 8. sayı süper yıldız ve 1 ile 90 arası random... 
