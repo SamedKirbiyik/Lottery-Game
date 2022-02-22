7613b82d682a97cb0592a2e83a648912a881d058
SamedKirbiyik <samedkirbiyik@gmail.com>
2022-02-22 16:08:44 +0300

    add

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
-let container = document.getElementById("container");
-let contentText = document.getElementById("contentText");
-let chanceNumber = document.getElementById("chanceNumber");
-let addButton = document.getElementById("addButton");
-let deleteButton = document.getElementById("deleteButton");
-let output = document.getElementById("output");
+let input = document.getElementById("input");
+let added = document.getElementById("added");
+let result = document.getElementById("result");
+let deleted = document.getElementById("deleted");
 
-addButton.addEventListener('click', function(){
+
+added.addEventListener('click', function(){
+    result.innerHTML = "";
+
+    var numbers = [];
+
     
+    for(let i =0; i < input.value; i++ )
+    {
+        for(var j = 0; numbers.length < 6; j++){
+            let number = Math.floor(Math.random() * 90)+1;
+            if(numbers.includes(number)){
+                continue
+            }
+            else{
+                numbers.push(number);
+            }
+        }
+        let sorted = number.sort((a,b) => a-b)
+
+        for (var k = 0; numbers.length < 7; k++){
+            let number = Math.floor(Math.random() * 90)+1;
+            if(numbers.includes(number)){
+                continue
+            }
+            else{
+                numbers.push(number);
+            }
+        }
+        numbers.push(Math.trunc(Math.random()*90)+1);
+
+        result.innerHTML += sorted[0]+ '|' + sorted[1]+ '|' +
+        sorted[2]+ '|' + sorted[3]+ '|' + sorted[4]+ '|' + sorted[5]+ '--'+
+        sorted[6]+ '--' + sorted[7]+ '|' +"<br>";
+
+        result.innerHTML += '<hr>';
+        numbers = []
+
+    }
+});
+deleted.addEventListener("click", ()=>{
+    input.value = "";
+    result.innerHTML = "";
 })
 
 
@@ -15,11 +54,3 @@ addButton.addEventListener('click', function(){
 
 
 
-
-// değişkenler sonrasında sayaç sıfırlanır
-// user girişi maks 8 ile sınırlanır.
-// if bloğuyla toplam şans numarası 6 ile sınırlanır. Ve küçükten büyüğe sıralanır.
-// Sayaç ++ her girişte
-// bu numaraların 1 ile 90 arasında olması math metoduyla sağlanır.
-// 7. numaranın joker olması nedeniyle ilk 6 numarayla !=
-// 8. sayı süper yıldız ve 1 ile 90 arası random... 
